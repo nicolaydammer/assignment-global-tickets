ddb26e862e186338c81a14732f5cb4666643df83
nicolaydammer <nicolaydammer@gmail.com>
2024-05-14 22:30:30 +0200

    added sass on template, moved a dependency and removed unused file

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @viteReactRefresh
     @vite('resources/js/App.jsx')
+    @vite('resources/sass/app.scss')
     @inertiaHead
 </head>
 <body>
